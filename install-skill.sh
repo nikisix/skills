fa40ae71ab7ec494af879a37dddce6fa5eafc615
Nick Tomasino <nicolai.tesela@gmail.com>
2026-06-09 15:33:30 -0700

    updating installer script

---
+++ install-skill.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# makes recursive symbolic links to the claude skills folder
+# can also run this piecewise to install individual skills
+if [ -d "$1" ]; then
+    rm -r ~/.claude/skills/"$1"
+fi
+mkdir -p ~/.claude/skills/"$1"
+# cp -srf "${PWD}"/skills/"$1"/.[^.]* ~/.claude/skills/"$1"/ # SOURCE must resolve to an absolute path to work
+cp -srf "${PWD}"/skills/"$1"/ ~/.claude/skills/"$1"/ # SOURCE must resolve to an absolute path to work
--- link.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# makes recursive symbolic links to the claude skills folder
-# can also run this piecewise to install individual skills
-cp -srf "${PWD}"/skills/.[^.]* ~/.claude/skills/ # SOURCE must resolve to an absolute path to work
